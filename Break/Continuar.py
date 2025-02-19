ad2fb512474f90057318b598daff2bf39bd53743
sergiomartinezo9710 <sergiomartinezo9710@gmail.com>
2025-02-19 10:41:12 -0500

    Delete Break directory

--- Break/Continuar.py
+++
@@ -1,5 +0,0 @@
-while True:
-    respuesta = input("¿Quieres continuar? (s/n): ")
-    if respuesta.lower() == 'n':                 #.lower-> Convierte las letras en minusculas. Otro tipo de dato los deja igual. 
-        print("¡Hasta luego!")
-        break   
