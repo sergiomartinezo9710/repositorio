72c293c9605c1f2128609584d8f63e57c0da9d61
sergiomartinezo9710 <sergiomartinezo9710@gmail.com>
2025-02-19 10:42:24 -0500

    Delete Examenes directory

--- Examenes/Examen1.py
+++
@@ -1,9 +0,0 @@
-# En una lista de numeros, retorna el segundo numero más grande
-
-lista = [5,7,2,7,9,4,8,11]
-lista.reverse()
-
-
-print(lista[-2])
-
->>>>>>> fba10b9b1fded8f84d8f537e6d4f9400c7c2552c
