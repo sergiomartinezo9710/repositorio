e5100ddaf1ab23e6f04c8cc93c07930b4efd890e
sergiomartinezo9710 <sergiomartinezo9710@gmail.com>
2025-02-19 10:16:22 -0500

    añadí html y css

---
+++ Javascript/java.js
@@ -0,0 +1,13 @@
+
+function calcularEdad(){
+    var edad=document.getElementById("edad").value;
+if (edad>=18){
+   alert("Tu puedes entrar hermoso, adelante ");
+} else{
+    alert("Tu no puedes entrar inmundo animal");
+    document.write("feo hpta !!");
+}
+}
+
+
+
